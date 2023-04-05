b67571b30ab1fc7452c45b888e1db8d42dbe3de5
Jonavinne Steadham <jasteads@gmail.com>
2023-04-05 00:25:24 -0500

    Adding SysManager file. This will start the game code when the program executes.

---
+++ Assets/Code/SysManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SysManager
+{
+    public static Camera mainCam;
+    public static readonly Font DEFAULT_FONT;
+    public static readonly Sprite DEFAULT_BUTTON;
+
+    static SysManager()
+    {
+        DEFAULT_FONT = Resources.GetBuiltinResource<Font>(
+            "Arial.ttf");
+        DEFAULT_BUTTON = null;
+
+        InterfaceTool.defaultFont = DEFAULT_FONT;
+    }
+
+    [RuntimeInitializeOnLoadMethod]
+    static void StartApplication()
+    {
+        mainCam = new GameObject("Main Camera")
+            .AddComponent<Camera>();
+        mainCam.tag = "MainCamera";
+        mainCam.orthographic = true;
+    }
+
+    public static void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+#endif
+        Application.Quit();
+    }
+}
