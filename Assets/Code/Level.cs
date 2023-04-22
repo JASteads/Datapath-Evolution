5cb6d9046706f8f8490171fcb8111f7d4b237bff
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2023-04-22 18:09:41 -0500

    levels can now optionally require control signals

--- Assets/Code/Level.cs
+++ Assets/Code/Level.cs
@@ -6,6 +6,7 @@ public class Level
 {
     private string name;
     private List<LevelObject> objects = new List<LevelObject>();
+    private Dictionary<string, int> requiredControlSignals = new Dictionary<string, int>(), currentControlSignals = new Dictionary<string, int>();
     private bool isLevelComplete = false;
 
     public Level(string name) {
@@ -21,7 +22,29 @@ public class Level
         objects.ForEach(obj => this.objects.Add(obj));
     }
 
+    public void setRequiredControlSignals(int regDst, int regWrite, int pcSrc, int aluSrc, int memRead, int memWrite, int memToReg) {
+        requiredControlSignals.Add("RegDST", regDst);
+        requiredControlSignals.Add("RegWrite", regWrite);
+        requiredControlSignals.Add("PCSrc", pcSrc);
+        requiredControlSignals.Add("ALUSrc", aluSrc);
+        requiredControlSignals.Add("MemRead", memRead);
+        requiredControlSignals.Add("MemWrite", memWrite);
+        requiredControlSignals.Add("MemToReg", memToReg);
+        //default all current to 0
+        foreach (KeyValuePair<string, int> signal in requiredControlSignals) {
+            currentControlSignals.Add(signal.Key, 0);
+        }
+    }
+
     public void CheckState() {
+        // never runs with no required signals
+        foreach (KeyValuePair<string, int> requiredSignal in requiredControlSignals) {
+            foreach (KeyValuePair<string, int> currentSignal in currentControlSignals) {
+                if (requiredSignal.Key == currentSignal.Key && requiredSignal.Value != currentSignal.Value) {
+                    return;
+                }
+            }
+        }
         objects.ForEach(obj => {
             obj.GetComponents().ForEach(component => {
                 if (component.GetCurrentState() != component.GetExpectedState()) {
@@ -30,7 +53,7 @@ public class Level
             });
         });
         isLevelComplete = true;
-        Debug.Log("level completed");
+        Debug.Log("Level " + name + " completed");
     }
 
     public string GetName() {
