606f5907b0daf0d1010003876c5385d4dc9f2c2a
Jonavinne Steadham <jasteads@gmail.com>
2023-04-26 00:48:57 -0500

    Created Tooltip Functionality, Etc.
    Also added prefab screens for main menu navigation

--- Assets/Code/Systems/Tooltip.cs
+++ Assets/Code/Systems/Tooltip.cs
@@ -4,5 +4,76 @@ using UnityEngine.EventSystems;
 
 public class Tooltip
 {
+    Transform tf;
+    Text headerTxt, bodyTxt;
+    bool isActive = false;
 
+    public Tooltip(Transform parent)
+    {
+        GameObject textBox = InterfaceTool.ImgSetup("Tooltip",
+            parent, out Image boxImg, false);
+        InterfaceTool.FormatRect(boxImg.rectTransform,
+            new Vector2(400, 700), new Vector2(0.5f, 0.5f),
+            new Vector2(0.5f, 0.5f), Vector2.zero);
+
+        headerTxt = InterfaceTool.CreateHeader("Sample Text",
+            textBox.transform, new Vector2(300, 50), new Vector2(-50, 0), 24);
+        bodyTxt = InterfaceTool.CreateBody("Insert text here",
+            textBox.transform, 14);
+
+        textBox.SetActive(false);
+    }
+
+    public void SetActive(bool _isActive)
+    {
+        isActive = _isActive;
+    }
+
+    public void AssignTooltip(Transform target)
+    {
+        EventTrigger.Entry enterEvent = new EventTrigger.Entry
+        { eventID = EventTriggerType.PointerEnter },
+            exitEvent = new EventTrigger.Entry
+            { eventID = EventTriggerType.PointerExit },
+            moveEvent = new EventTrigger.Entry
+            { eventID = EventTriggerType.Move };
+
+        enterEvent.callback.AddListener(data
+           => ShowTooltip(data as PointerEventData));
+        exitEvent.callback.AddListener(data
+            => HideTooltip(data as PointerEventData));
+        moveEvent.callback.AddListener(data => MoveTooltip());
+
+        EventTrigger eTrig = target.gameObject
+            .AddComponent<EventTrigger>();
+        eTrig.triggers.Add(enterEvent);
+        eTrig.triggers.Add(exitEvent);
+    }
+
+    void ShowTooltip(PointerEventData data)
+    {
+        if (!isActive) return;
+
+        TooltipLibrary.FetchInfo(data.pointerEnter.name,
+            out string newHeader, out string newBody);
+
+        headerTxt.text = newHeader;
+        bodyTxt.text = newBody;
+
+        tf.gameObject.SetActive(true);
+    }
+
+    void HideTooltip(PointerEventData data)
+    {
+        if (!isActive) return;
+
+        tf.gameObject.SetActive(false);
+    }
+
+    void MoveTooltip()
+    {
+        if (!tf.gameObject.activeSelf) return;
+
+        tf.localPosition = Input.mousePosition;
+    }
 }
---
+++ Assets/Code/Systems/TooltipLibrary.cs
@@ -0,0 +1,13 @@
+﻿public class TooltipLibrary
+{
+    public static void FetchInfo(string target, out string header, out string body)
+    {
+        header = body = null;
+        
+        switch (target)
+        {
+            default:
+                break;
+        }
+    }
+}
