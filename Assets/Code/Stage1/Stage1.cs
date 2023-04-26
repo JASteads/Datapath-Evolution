68021d0e3bb8192a06e62972d1a037748a33459f
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2023-04-26 17:13:01 -0500

    stage 1 complete

--- Assets/Code/Stage1/Stage1.cs
+++ Assets/Code/Stage1/Stage1.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 
 public class Stage1 : Level
 {
-    DropLocationList dList;
-
     public Stage1() : base("Stage 1")
     {   
         CreateIntroductionBox("In this stage, the player will need to assemble a single datapath by dragging different components into empty fields. The goal is to correctly assemble the datapath that will allow the user to understand the execution of datapath instructions.",
@@ -30,14 +28,42 @@ public class Stage1 : Level
 
     public void CreateStage1Objects()
     {
+        System.Random rand = new System.Random();
         List<DropLocation> dropLocations = new List<DropLocation>();
-        
-        for (int i = 0; i < 5; i++)
-        {
-            Stage1SlotObject obj = new Stage1SlotObject(levelObj, i, new Vector2((i * 250) - 375, 100), dropLocations);
+        int xPos = -500;
+        //slots
+        for (int i = 0; i < 5; i++) {
+            SlotObject obj = new SlotObject(levelObj, i, new Vector2((i * 250) + xPos, 100), dropLocations);
         }
-
+        //list
         DropLocationList dropLocationList = new DropLocationList(dropLocations);
+        //draggables
+        List<string> names = new List<string>{"PC", "Instruction Memory", "Register File", "ALU", "Data Memory"};
+        List<string> persistentNames = new List<string>{"PC", "Instruction Memory", "Register File", "ALU", "Data Memory"};
+        while (names.Count > 0) {
+            int index = rand.Next(names.Count);
+            string name = names[index];
+            DraggableObject obj = new DraggableObject(levelObj, name, persistentNames.IndexOf(name), dropLocationList, new Vector2(xPos, -200));
+            names.RemoveAt(index);
+            xPos += 250;
+        }
+        //valid check
+        GameObject winCheckObj = InterfaceTool.ButtonSetup("Check Indexes", levelObj.transform, out Image winCheckImg, out Button button, null, () => {
+            bool valid = true;
+            dropLocationList.dLocations.ForEach(dropLocation => {
+                if (!dropLocation.IsCorrectState()) {
+                    valid = false;
+                }
+            });
+            if (valid) {
+                ResetObjects();
+                SysManager.SetLevel(SysManager.GetStage2());
+            }
+        });
+        InterfaceTool.FormatRect(winCheckImg.rectTransform, new Vector2(180, 60), DEF_VEC, DEF_VEC, DEF_VEC, new Vector2(0, -400));
+        winCheckImg.color = Color.gray;
+        Text txt = InterfaceTool.CreateHeader("Check Indexes", winCheckImg.transform, new Vector2(60, 20), new Vector2(35, -40), 16);
+        txt.color = Color.black;
     }
 }
 
