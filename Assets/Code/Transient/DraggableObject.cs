90e95c34b2494919d27f3305c3e4ee1462852793
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2023-04-26 17:27:36 -0500

    stage 1 name fixes

--- Assets/Code/Transient/DraggableObject.cs
+++ Assets/Code/Transient/DraggableObject.cs
@@ -10,7 +10,7 @@ public class DraggableObject
     public DraggableObject(GameObject levelObj, string name, int currentSlot, DropLocationList dropLocationList, Vector2 position) {
         InterfaceTool.ImgSetup(name, levelObj.transform, out image, null, true);
         InterfaceTool.FormatRect(image.rectTransform, new Vector2(150, 150), position);
-        Text text = InterfaceTool.CreateHeader(name, GetTF(), new Vector2(0, 30), new Vector2(0, -90), 24);
+        Text text = InterfaceTool.CreateHeader(name, GetTF(), new Vector2(0, 60), new Vector2(0, -110), 24);
         text.alignment = TextAnchor.MiddleCenter;
         text.color = Color.black;
         new DragAndSnapEvent(image, dropLocationList, currentSlot);
