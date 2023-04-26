af4ba9bc95983e4c1ddf0d661fec61e49dff5399
Jonavinne Steadham <jasteads@gmail.com>
2023-04-26 14:26:55 -0500

    Removed DropLocation's object independence

--- Assets/Code/Systems/DragNSnap/DropLocation.cs
+++ Assets/Code/Systems/DragNSnap/DropLocation.cs
@@ -1,27 +1,17 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
 public class DropLocation
 {
     public static int UNSET = -1;
-    readonly Color locationColor = new Color(0, 0, 0, 0.4f);
+
     readonly Transform tf;
     readonly int expectedState;
-
     int state;
 
-    DropLocation(string name, RectTransform parent,
-        int _expectedState, Vector2 newPos)
+    DropLocation(Transform _target, int _expectedState)
     {
         expectedState = _expectedState;
-        tf = InterfaceTool.ImgSetup($"Drop Location {expectedState}",
-            parent, out Image img, true).transform;
-        InterfaceTool.FormatRect(img.rectTransform,
-            new Vector2(200, 200), new Vector2(0.5f, 0.5f),
-            new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f),
-            newPos);
-        img.color = locationColor;
-
+        tf = _target;
         state = UNSET;
     }
 
