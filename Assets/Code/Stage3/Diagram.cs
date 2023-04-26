5e4b2de632fecfb017f4eb48cf4e912ae6cdf755
Jonavinne Steadham <jasteads@gmail.com>
2023-04-26 13:12:40 -0500

    Updated Diagram Prefab

--- Assets/Code/Stage3/Diagram.cs
+++ Assets/Code/Stage3/Diagram.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Diagram : MonoBehaviour
 {
     public GameObject prefab;
+
+    // public Button leftRightButtons
 
     void Start()
     {
