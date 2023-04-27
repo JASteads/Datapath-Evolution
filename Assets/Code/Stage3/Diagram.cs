68c76aec55cf8d0f4f398e181583e630f5af211e
Jonavinne Steadham <jasteads@gmail.com>
2023-04-27 12:30:50 -0500

    MainMenu Extension
    Prefabs are no longer overwritten in Start()

--- Assets/Code/Stage3/Diagram.cs
+++ Assets/Code/Stage3/Diagram.cs
@@ -3,14 +3,43 @@ using UnityEngine.UI;
 
 public class Diagram : MonoBehaviour
 {
-    public GameObject prefab;
+    const int SLOT_COUNT = 4;
 
-    // public Button leftRightButtons
+    public GameObject prefab;
+    Slot[] slots;
 
     void Start()
     {
-        Instantiate(prefab);
+        const int INS_INDEX = 5;
 
-        // Add functionality to the buttons and movable slots
+        prefab = Instantiate(prefab);
+        
+        Button[] rightButtons = new Button[SLOT_COUNT],
+            leftButtons = new Button[SLOT_COUNT];
+        slots = new Slot[SLOT_COUNT];
+
+        for (int i = 0; i < SLOT_COUNT; i++)
+        {
+            int slotNum = i;
+
+            rightButtons[i] = prefab.transform.GetChild(INS_INDEX)
+                .GetChild(i).GetChild(0).GetComponent<Button>();
+            rightButtons[i].onClick.AddListener(() 
+                => MoveRight(slotNum));
+            leftButtons[i] = prefab.transform.GetChild(INS_INDEX)
+                .GetChild(i).GetChild(1).GetComponent<Button>();
+            leftButtons[i].onClick.AddListener(()
+                => MoveLeft(slotNum));
+        }
+    }
+
+    void MoveLeft(int slotNum)
+    {
+        
+    }
+
+    void MoveRight(int slotNum)
+    {
+
     }
 }
